0f789da0cc1f914274242bcae8fc70391501a124
Anoop Sharma <anoopksharma22@gmail.com>
2022-09-10 05:47:24 +0530

    code restructure

--- main.py
+++ main.py
@@ -1,48 +1,52 @@
 import argparse
-ap = argparse.ArgumentParser(description="Prints the csv file in tabular form")
 
+ap = argparse.ArgumentParser(description="Prints the csv file in tabular form")
 ap.add_argument("-f", "--file", required=True, help="csv file")
 args = vars(ap.parse_args())
 
 
-class ColWidth:
-    def __init__(self,col_width) -> None:
-        self.col_width = col_width
-    
-    def total_col_width(self):
+def read_file():
+    rows = []
+    col_width = {}
+    with open(args['file'],'r') as f:
+        while True:
+            row = f.readline().strip()
+            rows.append(row)
+            if not row:
+                break
+            
+            cols = row.split(',')
+            for i,col in enumerate(cols):            
+                if i in col_width:
+                    if col_width[i] < len(col):
+                        col_width[i] = len(col)
+                else:
+                    col_width[i] = len(col)
+    return rows,col_width
+
+def total_col_width(col_width):
         width = 0
-        for k,v in self.col_width.items():
+        for k,v in col_width.items():
             width += v
         
-        return width + len(self.col_width) * 3 + 1
+        return width + len(col_width) * 3 + 1 #Not sure about adding this 1
 
-with open(args['file'],'r') as f:
-    col_width = {}
-    rows = []
-    while True:
-        row = f.readline().strip()
-        rows.append(row)
-        if not row:
-            break
-        
-        cols = row.split(',')
-        for i,col in enumerate(cols):            
-            if i in col_width:
-                if col_width[i] < len(col):
-                    col_width[i] = len(col)
-            else:
-                col_width[i] = len(col)
-
-    colw = ColWidth(col_width)
-    x = '-' * colw.total_col_width()
+def run():
+    rows, col_width = read_file()    
+    x = '-' * total_col_width(col_width)
     for i,row in enumerate(rows):
         if not row:
             break
         cols = row.split(',')
-        print(f'{x:<{colw.total_col_width()}}')
+        print(f'{x:<{total_col_width(col_width)}}')
         for i,col in enumerate(cols):
             print(f'| {col:<{col_width[i]}} ',end='')
         print("|",end='')
         print()
+
+    print(f'{x:<{total_col_width(col_width)}}')
+
+
+if __name__ == "__main__":
+    run()
     
-    print(f'{x:<{colw.total_col_width()}}')
